27360f7b56692c08fd8e79e573e4977d1926fa7c
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-09 09:12:59 +0000

    first draft

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ const blogPostsRouter = require('./blogPostsRouter');
 
 app.use(morgan('common'));
 
-// Need to set this up!
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-													// Need to create this!
 	res.sendFile(__dirname + '/views/index.html');
 });
 
-					// Need to set this up!
 app.use('./blog-posts', blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
-							// Play with this to see what's going on in the ${} brackets!
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 })
